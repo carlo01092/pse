2f13d22fd99d6205d4089e1f1681af409cd78289
carlo01092 <70194749+carlo01092@users.noreply.github.com>
2020-08-29 02:22:30 +0800

    updated up to August 28 2020

--- ohlcv_json.ps1
+++ ohlcv_json.ps1
@@ -1,15 +1,18 @@
 $total_saves = 0;
+$stocks_counter = 0;
 $current_date_unix = [int][double]::Parse((Get-Date -Date "12:00:00 AM" -UFormat %s))
 
 Get-Content .\stock.txt | ForEach-Object {
     $response = Invoke-WebRequest -Uri `
         "https://api.kisschart.com/api/chart/history?symbol=$_&resolution=D&from=473472000&to=$current_date_unix"
+    ++$stocks_counter
     if ($response.StatusCode -eq 200) {
         $ohlcv = $response.Content | ConvertFrom-Json
 
         if ($ohlcv.s -eq "ok") {
             $ohlcv.PSObject.Properties.Remove('s')
-            ($ohlcv | ConvertTo-Json) -replace '\s','' | Out-File -FilePath .\json\$_.json
+            $ohlcv_bytes =  [System.Text.Encoding]::UTF8.GetBytes(($ohlcv | ConvertTo-Json -Compress) + [Environment]::NewLine)
+            [System.IO.File]::WriteAllBytes("$PSScriptRoot\json\$_.json", $ohlcv_bytes)
             "$_ saved"
             ++$total_saves
         } else {
@@ -20,4 +23,4 @@ Get-Content .\stock.txt | ForEach-Object {
     }
 }
 
-"$total_saves total saves"
+"$total_saves/$stocks_counter total saves"
