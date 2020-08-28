38d77d17bba8fe919063ca5d21a5357271e19998
carlo01092 <70194749+carlo01092@users.noreply.github.com>
2020-08-29 07:05:38 +0800

    updated up to August 28 2020

--- pdf_report.ps1
+++ pdf_report.ps1
@@ -1,12 +1,38 @@
-﻿$url = "https://www.pse.com.ph/stockMarket/marketInfo-marketActivity-marketReports.html?ajax=true&method=downloadMarketReports&ids=[%22PSE_DQTRT20204129%22]"
+$counter = 0;
+$starting_id = 3219 #December 1 2016
+$year = 2016
+$last_id = 4131 #August 28 2020, $counter == 910
 
-$response = Invoke-WebRequest `
-    -Uri "$url" `
-    -Headers @{
-        'Referer'= 'https/www.pse.com.ph/stockMarket/marketInfo-marketActivity.html?tab=5';
-    } `
-    -Method Get
+$id_border = 3239, 3482, 3727, 3971
 
-$file_name = [System.Net.Mime.ContentDisposition]::new($response.Headers['Content-Disposition']).FileName
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-[System.IO.File]::WriteAllBytes("C:\Users\CHARLES\Downloads\dev\$file_name", $response.Content)
+for ($id = $starting_id; $id -le $last_id; $id++) {
+    if ($id -in $id_border) {
+        ++$year
+    }
+
+    $url = "https://www.pse.com.ph" `
+        +"/stockMarket/marketInfo-marketActivity-marketReports.html" `
+        +"?ajax=true&method=downloadMarketReports&ids=[%22PSE_DQTRT$year$id%22]"
+
+    $response = Invoke-WebRequest `
+        -Uri "$url" `
+        -Headers @{
+            'Referer'= 'https/www.pse.com.ph/stockMarket/marketInfo-marketActivity.html?tab=4';
+        } `
+        -Method Get
+
+    try {
+        $file_name = [System.Net.Mime.ContentDisposition]::new($response.Headers['Content-Disposition']).FileName
+        [System.IO.File]::WriteAllBytes("$PSScriptRoot\pdf\$file_name", $response.Content)
+    }
+    catch [System.IndexOutOfRangeException], [System.Management.Automation.MethodException] {
+        continue
+    }
+
+    "$file_name saved ($year$id)"
+    ++$counter
+}
+
+"$counter total saves"
