4f4c069a2246971dfef837ea937b5a1cbae79499
carlo01092 <70194749+carlo01092@users.noreply.github.com>
2020-08-29 02:08:43 +0800

    rename files

---
+++ pdf_reader.ps1
@@ -0,0 +1,11 @@
+﻿Add-Type -path "itextsharp.dll"
+$file = "$PWD\stockQuotes_08262020.pdf"
+$pdf = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList "$file"
+[iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($pdf, 1)
+
+#for ($page = 1; $page -le 1; $page++) {
+#    $texto = [iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($pdf,$page)
+#   Write-Output $texto
+#}
+
+$pdf.Close()
